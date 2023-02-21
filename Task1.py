17a1407029cfc1de606fb53388911b426520ddf4
Ivan Minakov <i.minakov786@gmail.com>
2023-02-21 17:38:37 -0500

    done

--- Task1.py
+++ Task1.py
@@ -1,21 +1,57 @@
-def GetVowelsCount(text):
-    count = 0
-    for letter in text:
-        if letter in vowels:
-            count = count + 1
-    return count
+phone_book = {}
 
+def add_contact():
+    name = input("Введите имя: ")
+    phone = input("Введите номер телефона: ")
+    phone_book[name] = phone
+    print(f"Контакт {name} успешно добавлен в справочник.")
 
-vowels = ['ё', 'у', 'е', 'ы', 'а', 'о', 'э', 'я', 'и', 'ю']
-verse = input("text = ").lower()
-phrases = verse.split(' ')
+def edit_contact():
+    name = input("Введите имя контакта, который хотите отредактировать: ")
+    if name in phone_book:
+        phone = input("Введите новый номер телефона: ")
+        phone_book[name] = phone
+        print(f"Контакт {name} успешно изменен.")
+    else:
+        print(f"Контакт {name} не найден в справочнике.")
 
-vowelsCount = GetVowelsCount(phrases[0])
-isVerseOk = True
-for phrase in phrases:
-    isVerseOk &= GetVowelsCount(phrase) == vowelsCount
+def delete_contact():
+    name = input("Введите имя контакта, который хотите удалить: ")
+    if name in phone_book:
+        del phone_book[name]
+        print(f"Контакт {name} успешно удален.")
+    else:
+        print(f"Контакт {name} не найден в справочнике.")
 
-if isVerseOk:
-    print("Парам пам-пам")
-else:
-    print("Пам парам")
+def print_contacts():
+    if phone_book:
+        print("Список контактов:")
+        for name, phone in phone_book.items():
+            print(f"{name}: {phone}")
+    else:
+        print("Справочник пуст.")
+
+def main():
+    while True:
+        print("Меню:")
+        print("1. Добавить контакт")
+        print("2. Редактировать контакт")
+        print("3. Удалить контакт")
+        print("4. Показать все контакты")
+        print("5. Выйти")
+        choice = input("Введите номер действия: ")
+        if choice == "1":
+            add_contact()
+        elif choice == "2":
+            edit_contact()
+        elif choice == "3":
+            delete_contact()
+        elif choice == "4":
+            print_contacts()
+        elif choice == "5":
+            break
+        else:
+            print("Некорректный ввод. Попробуйте еще раз.")
+
+if __name__ == '__main__':
+    main()
